20b32bdf5e3ae6cb873fa5008509e1a493cfafdd
Rafe Hart <rafael.hart9@gmail.com>
2019-06-05 11:00:42 +1000

    Starting management of the environment with pipenv

--- cost_report.py
+++ cost_report.py
@@ -1,4 +1,13 @@
 from settings import AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_SUBSCRIPTION_ID
+import azure.mgmt.managementgroups
+
+# This script expects that the following environment vars are set in settings.py:
+#
+# AZURE_TENANT_ID: with your Azure Active Directory tenant id or domain
+# AZURE_CLIENT_ID: with your Azure Active Directory Application Client ID
+# AZURE_CLIENT_SECRET: with your Azure Active Directory Application Secret
+# AZURE_SUBSCRIPTION_ID: with your Azure Subscription Id
+#
 
 if __name__ == "__main__":
     print(AZURE_TENANT_ID)
